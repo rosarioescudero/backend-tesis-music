799c135991d58e4ff1aaa16ca128f4fb44b00ce9
rosarioescudero <rosario.escudero@mi.unc.edu.ar>
2026-04-24 10:04:50 -0300

    Update analisis_api.py

--- analisis_api.py
+++ analisis_api.py
@@ -282,5 +282,6 @@ def analizar():
 
 
 if __name__ == "__main__":
-    print("Servidor de Python activo en http://localhost:5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    print("Servidor de Python activo")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
